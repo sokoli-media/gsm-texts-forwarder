fb912daa7d160462dae327ba9502a020b71098c7
Maciej Plonski <info@sokoli.pl>
2025-12-18 16:10:57 +0100

    change comments to log lines

--- scripts/forward_sms.sh
+++ scripts/forward_sms.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "starting forwarder..."
+
 if [ -z "$GAMMU_DEVICE" ]; then
   echo "GAMMU_DEVICE is not set"
   exit 1
@@ -20,6 +22,8 @@ device = $GAMMU_DEVICE
 connection = at
 EOF
 
+echo "config has been updated"
+
 count=$(gammu --getfolders | grep -i 'Inbox' | awk '{print $4}')
 
 if [ "$count" -ge 1 ]; then
@@ -34,3 +38,5 @@ if [ "$count" -ge 1 ]; then
     gammu deletesms INBOX $i
   done
 fi
+
+echo "forwarder has finished"
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# switch Huawei modem to modem mode
+echo "switching Huawei modem to modem mode..."
 usb_modeswitch -v 12d1 -p 1446 -J || true
 
-# wait a few seconds for ttyUSB devices to appear
+echo "waiting for the modem to load..."
 sleep 5
 
-# start cron
+echo "starting cron..."
 cron -f
